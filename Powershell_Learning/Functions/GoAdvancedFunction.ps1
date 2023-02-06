cdd17ff43a7c98c498ec58ea69af88f71580034b
Brian Hobbs <v-brianhobbs@microsoft.com>
2023-02-06 15:54:07 -0800

    More updates

--- Powershell_Learning/Functions/GoAdvancedFunction.ps1
+++ Powershell_Learning/Functions/GoAdvancedFunction.ps1
@@ -1,23 +1,37 @@
-# CmdletBinding attribute: 
-# --Add basic cmdlet features, such as common parameters, to functions 
-# --Make certain methods available that allow you to change the behavior of the function.
-Function Copy-OneTwo {
-    Param(
-            [Parameter(Mandatory=$True,
-                      Position=0)]
-            [String]$Source,
-            [Parameter(Mandatory=$True,
-                      ParameterSetName="OneDestination")]
-            [String]$Destination,
-            [Parameter(Mandatory=$True,
-                      ParameterSetName="TwoDestinations")]
-            [String]$Destination1,   
-            [Parameter(Mandatory=$True,
-                      ParameterSetName="TwoDestinations")]
-            [String]$Destination2
-         )
-     switch ($PsCmdlet.ParameterSetName){
-        "OneDestination"   { Copy-Item -Recurse $Source $Destination } 
-        "TwoDestinations"  { Copy-Item -Recurse $Source $Destination1; Copy-Item $Source $Destination2 }
-     }
+<#
+    .SYNOPSIS
+        Placeholder for now
+    
+    .PARAMETER ComputerName
+    The remote computer(s) to check the status of the services on.
+
+    .EXAMPLE
+        Dot source to reference code defined in one script in a separate one
+        . .\GoAdvancedFunction
+        Ping-Connection -ComputerName $vm
+
+    .NOTES
+         Author: Brian Hobbs
+#>
+function Ping-Connection {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [string[]]$ComputerName
+    )
+
+    PROCESS {
+        foreach ($Computer in $ComputerName) {
+            try {
+                Test-Connection -IPv4 -Ping -ResolveDestination $Computer 
+            }
+            catch {
+                Write-Warning -Message "Unable to connect to Computer: $Computer"
+            }
+        }
+    }
+
 }
