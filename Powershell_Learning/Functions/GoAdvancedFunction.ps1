bda36ea576f22cbe3d8f140a17c7e1914a194fde
Brian Hobbs <v-brianhobbs@microsoft.com>
2023-02-06 13:18:01 -0800

    Save All

--- Powershell_Learning/Functions/GoAdvancedFunction.ps1
+++ Powershell_Learning/Functions/GoAdvancedFunction.ps1
@@ -1,10 +1,21 @@
 # CmdletBinding attribute: 
 # --Add basic cmdlet features, such as common parameters, to functions 
 # --Make certain methods available that allow you to change the behavior of the function.
-
-Function GoVerbose {
-    [CmdletBinding()]Param()
-    Write-Verbose "Alright, you prefer talkative functions. First of all, I appreciate your wish to learn more about the common parameter -Verbose. Secondly, blah blah.."
-    Write-Host "This is self-explanatory, anyway."
-}
-GoVerbose
+Function Copy-OneTwo {
+    Param(
+            [Parameter(Mandatory=$True,
+                      Position=0)]
+            [String]$Source,
+            [Parameter(Mandatory=$True,
+                      ParameterSetName="OneDestination")]
+            [String]$Destination,
+            [Parameter(Mandatory=$True,
+                      ParameterSetName="TwoDestinations")]
+            [String]$Destination1,   
+            [Parameter(Mandatory=$True,
+                      ParameterSetName="TwoDestinations")]
+            [String]$Destination2
+         )
+     switch ($PsCmdlet.ParameterSetName){
+        "OneDestination"   { Copy-Item -Recurse $Source $Destination } 
+        "TwoDestinations"  { Copy-Item -Recurse $Source $Destination1; Copy-Item $Source $Destination2 }
